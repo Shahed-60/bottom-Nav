2d059f994ac8a5cc2300dbabfdab685bc3415719
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-19 16:47:19 +0200

    final commit

---
+++ navigation/screens/DetailsScreen.js
@@ -0,0 +1,13 @@
+import * as Reast from 'react';
+import { View, Text } from 'react-native';
+
+export default function DetailsScreen(navigation) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+                onPress={() => navigation.navigate('Home')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}
+            >Detalis Screen</Text>
+        </View>
+    )
+}
